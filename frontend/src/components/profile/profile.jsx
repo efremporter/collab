02710f39c0a710812191141a1edeca24e322b4c7
Efrem <eopiscool@gmail.com>
2022-02-08 09:24:56 -0800

    Create profile

---
+++ frontend/src/components/profile/profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Profile</h2>
+        
+      </div>
+    )
+  }
+}
+
+export default Profile
