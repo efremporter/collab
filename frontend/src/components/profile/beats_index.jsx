d4cf47aad637a319768fd6837ba88c712db53962
Efrem <eopiscool@gmail.com>
2022-02-08 09:36:53 -0800

    Main page

---
+++ frontend/src/components/profile/beats_index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class BeatsIndex extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>This is the Beats index</div>
+    )
+  }
+}
