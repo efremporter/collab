0a8582e01961787027d0c772659a8ef2840e6bed
Efrem <eopiscool@gmail.com>
2022-02-16 11:47:52 -0800

    Styling

--- frontend/src/actions/comment_actions.js
+++ frontend/src/actions/comment_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from "../util/comments_util";
 
 export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS';
 export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
 
 
 const receiveComment = comment => {
@@ -18,6 +19,13 @@ const receiveComments = comments => {
   }
 }
 
+const deleteComment = commentId => {
+  return {
+    type: DELETE_COMMENT,
+    commentId
+  }
+}
+
 export const fetchComments = beatId => dispatch => {
   return APIUtil.fetchComments(beatId)
     .then( comments => dispatch(receiveComments(comments.data)))
@@ -26,4 +34,9 @@ export const fetchComments = beatId => dispatch => {
 export const createComment = comment => dispatch => {
   return APIUtil.createComment(comment)
     .then( comment => dispatch(receiveComment(comment.data)))
+}
+
+export const removeComment = commentId => dispatch => {
+  return APIUtil.deleteComment(commentId)
+  .then( comment => dispatch(deleteComment(commentId)))
 }
