b59505a93d2833b79ea856baaf738123e7af3b4c
zack alsiday <alsidayzack@gmail.com>
2022-02-18 14:59:40 -0800

    styling

--- frontend/src/components/profile/post_beat_form.jsx
+++ frontend/src/components/profile/post_beat_form.jsx
@@ -44,7 +44,7 @@ class PostBeatForm extends React.Component {
       <form className="profile-beat-form" action="">
         <div className='profile-post-beat-title'>Post a Beat</div>
         <div className='profile-post-beat-container'>
-          <input className='profile-post-beat-upload' type="file" onChange={this.handleChange('file')}></input>
+          <div className="upload-but"><input className='profile-post-beat-upload' type="file" onChange={this.handleChange('file')}></input></div>
           <input className='profile-post-beat-input' type="text" placeholder="Title Your Beat" value={this.state.title} onChange={this.handleChange('title')}></input>
           <button className='profile-post-beat-submit-but' onClick={this.handleSubmit.bind(this)}>Submit</button>
           <div className='profile-post-beat-errors-container'>
